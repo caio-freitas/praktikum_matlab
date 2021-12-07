036719f22009684293e70830bd4c4a6dbc5f8f16
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2021-12-07 21:54:55 +0100

    Lineasirung check, mehr über die Notebook gemacht

--- 2_versuch/linearisierung.m
+++ 2_versuch/linearisierung.m
@@ -1,14 +1,14 @@
-function [A, B, C, D] = linearisierung(f, h, AP)
+function [A_AP1, B_AP1, C_AP1, D_AP1] = linearisierung(f, h, AP)
     syms phi1 dphi1 phi2 dphi2
     x=[phi1, dphi1, phi2, dphi2];
     u=[];
-    A = jacobian(f, x) 
-    B = jacobian(f, u)
-    C = jacobian(h, x) 
-    D = jacobian(h, u)
+    A = jacobian(f, x); 
+    B = jacobian(f, u);
+    C = jacobian(h, x);
+    D = jacobian(h, u);
 
-    A = subs(A,x,AP)
-    B = subs(B,u,AP)
-    C = subs(C,x,AP)
-    D = subs(D,u,AP)
+    A_AP1 = subs(A,x,AP);
+    B_AP1 = subs(B,u,AP);
+    C_AP1 = subs(C,x,AP);
+    D_AP1 = subs(D,u,AP);
 end
