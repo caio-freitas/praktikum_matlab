0757c94c4063c9081957a5303b46dfc67a1fade3
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2021-12-07 21:14:05 +0100

    linearisierung ist gut jetzt, aber ich bin nicht über wird etwas für haben nächste

--- 2_versuch/linearisierung.m
+++ 2_versuch/linearisierung.m
@@ -1,13 +1,14 @@
 function [A, B, C, D] = linearisierung(f, h, AP)
-    syms phi1 dphi1 phi2 dphi2;
-    A = jacobian(f, [phi1;dphi1;phi2;dphi2]);
-    B = jacobian(f, u); %was ist u?
-    C = jacobian(h, [phi1;dphi1;phi2;dphi2]));
-    D = jacobian(h, u); %was ist u?
+    syms phi1 dphi1 phi2 dphi2
+    x=[phi1, dphi1, phi2, dphi2];
+    u=[];
+    A = jacobian(f, x) 
+    B = jacobian(f, u)
+    C = jacobian(h, x) 
+    D = jacobian(h, u)
 
-    phi1 = AP(1);
-    dphi1 = AP(2);
-    phi2 = AP(3);
-    dphi2 = AP(4);
-    subs(A)
+    A = subs(A,x,AP)
+    B = subs(B,u,AP)
+    C = subs(C,x,AP)
+    D = subs(D,u,AP)
 end
