1f81f255168ebd5335ab0f7bfe1a6b57d4cecdb2
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2021-12-12 16:19:14 +0100

    lineasierung corerct

--- 2_versuch/linearisierung.m
+++ 2_versuch/linearisierung.m
@@ -1,14 +1,16 @@
 function [A_AP1, B_AP1, C_AP1, D_AP1] = linearisierung(f, h, AP)
-    syms phi1 dphi1 phi2 dphi2
+    syms phi1 dphi1 phi2 dphi2 M
     x=[phi1, dphi1, phi2, dphi2];
-    u=[];
+    u=[M];
+    
+    conditions=[phi1==AP(1), dphi1==AP(2), phi2==AP(3),dphi2==AP(4)]
     A = jacobian(f, x); 
     B = jacobian(f, u);
     C = jacobian(h, x);
     D = jacobian(h, u);
 
     A_AP1 = subs(A,x,AP);
-    B_AP1 = subs(B,u,AP);
+    B_AP1 = subs(B,x,AP);
     C_AP1 = subs(C,x,AP);
-    D_AP1 = subs(D,u,AP);
+    D_AP1 = subs(D,x,AP);
 end
