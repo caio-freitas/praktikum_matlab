af2cee24f50344fa7012c5d7c7d5451e8a2f7ec9
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2021-12-12 18:20:39 +0100

    gilbert corrected

--- 2_versuch/checkCtrbGilbert.m
+++ 2_versuch/checkCtrbGilbert.m
@@ -1,4 +1,10 @@
 function checkCtrbGilbert(A, B)
     [A_til, B_til, C_til, D_til] = diagonalForm(A, B, eye(max(size(A))), eye(max(size(A))));
-    rank(B_til) == size(B_til, 2)
+    gilbertCtrb = (rank(B_til) == size(B_til, 2));
+    for i=1:size(B_til,1)
+        if all(B_til(i,:)==0)
+            gilbertCtrb = false;
+        end
+    end
+    gilbertCtrb
 end
