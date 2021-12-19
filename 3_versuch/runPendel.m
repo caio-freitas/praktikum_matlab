be726c16faa6c1d0538a9b094dd50f430e469705
Caio Freitas <caiofreitas@usp.br>
2021-12-19 19:22:00 +0100

    aufgabe 3

--- 3_versuch/runPendel.m
+++ 3_versuch/runPendel.m
@@ -1,4 +1,10 @@
 function [vT, mX, vU] = runPendel(stPendel, AP, K,  x0)
-
-end
+    [f, h] = nonlinear_model();
+    [A_AP, B_AP, C_AP, D_AP] = linearisierung(f,h,AP);
     
+    stOptions = simset( 'SrcWorkspace', 'current' );    
+    sim("Regelung_V3.slx", 10, stOptions);
+    vT = mZustand.Time;
+    mX = mZustand.Data;
+    vU = vInput.Data;
+end
