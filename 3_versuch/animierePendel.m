4c61e346075775e0da0c88789e92813ee5ff7a5b
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2021-12-19 20:40:03 +0100

    aniemere working

--- 3_versuch/animierePendel.m
+++ 3_versuch/animierePendel.m
@@ -8,21 +8,23 @@ function animierePendel(vT, mX, stPendel, hAxes)
     phi1 = mX(:,1);
 	phi2 = mX(:,3);
     
-    phi1Interp = interp1(phi1,vT,vTAnim);
-    phi2Interp = interp1(phi2,vT,vTAnim);
+    phi1Interp = interp1(vT, phi1,vTAnim);
+    phi2Interp = interp1(vT,phi2,vTAnim);
     
-    x1   = l1/2*sin(phi1Interp);
-    y1   = -l1/2*cos(phi1Interp);
-    x2   = l1*sin(phi1Interp) + l2/2*sin(phi2Interp);
-    y2   = -l1*cos(phi1Interp) + -l2/2*cos(phi2Interp);
+    x1   = l1*sin(phi1Interp);
+    y1   = -l1*cos(phi1Interp);
+    x2   = l1*sin(phi1Interp) + l2*sin(phi2Interp);
+    y2   = -l1*cos(phi1Interp) + -l2*cos(phi2Interp);
     
     p1 = @(i) plot([0 x1(i)],[0 y1(i)],'b-');
     p2 = @(i) plot([x1(i) x2(i)],[y1(i) y2(i)],'r-');
     
-    indexes=linspace(0,vTAnim(end),size(vTAnim));
+    indexes=[1,size(vTAnim,2)];
 
+    figure
+    axis(hAxes)
     hold on;
-    fanimator(p1,'AnimationRange',indexes);
-    fanimator(p2,'AnimationRange',indexes);
-    fanimator(@(t) text(-0.3,0.3,"Timer: "+num2str(vTAnim(t),2)+" s"),'AnimationRange',indexes);
+    fanimator(p1,'AnimationRange',indexes,'FrameRate',1);
+    fanimator(p2,'AnimationRange',indexes,'FrameRate',1);
+    fanimator(@(t) text(-0.1,0.3,"Timer: "+num2str(vTAnim(t),2)+" s"),'AnimationRange',indexes,'FrameRate',1);
 end 
