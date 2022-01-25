3d5313a9533cf665a33bbd09c7aace3a05c2a463
Caio Freitas <caiofreitas@usp.br>
2022-01-25 22:01:06 +0100

    linearisierung_XU

--- 6_versuch/linearisierung_XU.m
+++ 6_versuch/linearisierung_XU.m
@@ -1,3 +1,17 @@
 function [A, B, C, D] = linearisierung_XU(x_T, u_T)
+    syms phi1 dphi1 phi2 dphi2 M
+    x=[phi1, dphi1, phi2, dphi2];
+    u=[M];
+    
+    [f, h] = nonlinear_model();
 
+    Asim = jacobian(f, x);
+    Bsim = jacobian(f, u);
+    Csim = jacobian(h, x);
+    Dsim = jacobian(h, u);
+
+    A = double(subs(subs(Asim, x, x_T), u, u_T));
+    B = double(subs(subs(Bsim, x, x_T), u, u_T));
+    C = double(subs(subs(Csim, x, x_T), u, u_T));
+    D = double(subs(subs(Dsim, x, x_T), u, u_T)); 
 end
